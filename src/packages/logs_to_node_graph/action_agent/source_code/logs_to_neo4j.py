68666d689dede850bb362026cef500ce86952482
Daniel King <daniel.king@xmpro.com>
2024-10-23 11:05:13 +1000

    modified script

--- src/packages/logs_to_node_graph/action_agent/source_code/logs_to_neo4j.py
+++ src/packages/logs_to_node_graph/action_agent/source_code/logs_to_neo4j.py
@@ -146,8 +146,7 @@ def _merge_sh_collection_and_sh_edge(tx, event):
         MATCH (sh:StreamHost {id: $sh_id})
     
         MERGE (c:Collection {id: $collection_id})
-        SET c.title = $collection_title,
-            c.last_updated = $last_updated
+        SET c.last_updated = $last_updated
 
         WITH sh, c
         
@@ -162,7 +161,6 @@ def _merge_sh_collection_and_sh_edge(tx, event):
     tx.run(query, {
         "sh_id": event['stream_host_id'],
         "collection_id": event['collection_id'],
-        "collection_title": event['collection_title'],
         "collection_rel_id": event['collection_id'] + "_" + event['stream_host_id'],
         "last_updated": datetime.now().isoformat()
     })
