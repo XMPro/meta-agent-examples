5f24f0bd91ccad5374bdc42af965c1ff8b739a07
Daniel King <daniel.king@xmpro.com>
2024-10-23 12:04:00 +1000

    modified script

--- src/packages/logs_to_node_graph/action_agent/source_code/meta_action_agent.py
+++ src/packages/logs_to_node_graph/action_agent/source_code/meta_action_agent.py
@@ -3,17 +3,18 @@ from src.packages.logs_to_node_graph.action_agent.source_code.logs_to_neo4j impo
 from datetime import time
 
 def on_create(data: dict) -> dict | None:
-    update_frequency_in_seconds = data.get('update_frequency_in_seconds', 60)
+    # update_frequency_in_seconds = data.get('update_frequency_in_seconds', 60)
 
-    while True:
-        events = update_node_graph('/app/logs')
-        print(events)
-        time.sleep(update_frequency_in_seconds)
+    # while True:
+    #     events = update_node_graph('/app/logs')
+    #     print(events)
+    #     time.sleep(update_frequency_in_seconds)
+    return {}
 
 
 def on_receive(data: dict) -> dict | None:
-
-    return {}
+    events = update_node_graph('/app/logs')
+    return events
 
 
 def on_destroy() -> dict | None:
