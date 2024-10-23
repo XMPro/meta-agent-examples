4017c166bd1a7d5b444226355bfdfae38678a31b
Daniel King <daniel.king@xmpro.com>
2024-10-23 10:50:22 +1000

    modified script

--- src/packages/logs_to_node_graph/action_agent/source_code/logs_to_neo4j.py
+++ src/packages/logs_to_node_graph/action_agent/source_code/logs_to_neo4j.py
@@ -20,9 +20,18 @@ def update_node_graph(log_directory):
     events = _read_events(latest_log_file)
     _update_neo4j_node_graph(events)
 
-
     return events
 
+def _update_neo4j_node_graph(events: dict):
+
+    for key, event in events.items():
+
+        if (event['stream_object_id'] is None):
+            continue
+
+        with driver.session() as session:
+            session.write_transaction(_merge_stream_object, event)
+            session.write_transaction(_merge_sh_and_so_edge, event)
 
 def _get_latest_log_file(log_directory, pattern="sh-log-*.json"):
     """
@@ -32,10 +41,12 @@ def _get_latest_log_file(log_directory, pattern="sh-log-*.json"):
     log_files = glob.glob(os.path.join(log_directory, pattern))
 
     if not log_files:
-        raise FileNotFoundError(f"No log files found matching pattern '{pattern}' in {log_directory}")
+        raise FileNotFoundError(f"No log files found matching pattern '{
+                                pattern}' in {log_directory}")
 
     latest_file = max(log_files, key=os.path.getctime)
     return latest_file
+
 
 def _read_events(log_file_path):
     events = defaultdict(lambda: {
@@ -46,9 +57,9 @@ def _read_events(log_file_path):
         'stream_object_id': None,
         'stream_object_name': None,
         'stream_object_type': None,
-        'sh_id': None,
-        'sh_name': None,
-        'sh_collection_id': None
+        'stream_host_id': None,
+        'stream_host_name': None,
+        'collection_id': None
     })
 
     with open(log_file_path, 'r') as file:
@@ -70,9 +81,9 @@ def _read_events(log_file_path):
                 events[key]['stream_object_id'] = agent.get('Id')
                 events[key]['stream_object_name'] = agent.get('Name')
                 events[key]['stream_object_type'] = agent.get('Type')
-                events[key]['sh_id'] = host.get('DeviceId')
-                events[key]['sh_name'] = host.get('Name')
-                events[key]['sh_collection_id'] = host.get('CollectionId')
+                events[key]['stream_host_id'] = host.get('DeviceId')
+                events[key]['stream_host_name'] = host.get('Name')
+                events[key]['collection_id'] = host.get('CollectionId')
 
             except json.JSONDecodeError:
                 print(f"Error decoding JSON from line: {line}")
@@ -80,17 +91,6 @@ def _read_events(log_file_path):
                 print(f"KeyError: {e} in line: {line}")
 
     return dict(events)
-
-
-def _update_neo4j_node_graph(events: dict):
-    
-    for key, event in events.items():
-        
-        if(event['stream_object_id'] is None):
-            continue
-        
-        with driver.session() as session:
-            session.write_transaction(_merge_stream_object, event)
 
 
 def _merge_stream_object(tx, event):
@@ -109,4 +109,33 @@ def _merge_stream_object(tx, event):
         "so_event_count": event['count'],
         "log_created": event['timestamp'],
         "last_updated": datetime.now().isoformat()
+    })
+
+
+
+def _merge_sh_and_so_edge(tx, event):
+    query = """
+        MATCH (so:StreamObject {id: $so_id})
+    
+        MERGE (sh:StreamHost {id: $sh_id})
+        SET sh.title = $sh_title,
+            sh.last_updated = $last_updated
+
+        WITH so, sh
+
+        MERGE (sh)-[shr:EXECUTED { 
+                id: $sh_rel_id, 
+                source_id: $sh_id,
+                target_id: $so_id,
+                last_updated: $last_updated
+            }]->(so)
+    """
+
+    tx.run(query, {
+        "so_id": event['stream_object_id'],
+        "sh_rel_id": event['stream_host_id'] + "_" + event['stream_object_id'],
+        "sh_id": event['stream_host_id'],
+        "sh_title": event['stream_host_name'],
+        "log_created": event['timestamp'],
+        "last_updated": datetime.now().isoformat(),
     })
